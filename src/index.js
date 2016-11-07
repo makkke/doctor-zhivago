42f86dfeb0cf425d2e4f4bb660195a2cc60dd1b9
stanley288 <stanley.zhao@surrey.ca>
2016-11-07 15:49:54 -0800

    middle ware is done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import fetch from 'node-fetch'
+import { Router } from 'express'
+import { OK, SERVICE_UNAVAILABLE } from 'http-status'
 
 if (!global._babelPolyfill) {
   require('babel-polyfill')
@@ -13,5 +15,45 @@ export const apiCheck = async (url) => {
 }
 
 export const mongoCheck = async (mongoose) => {
-  return (mongoose.connection.readyState === 1)
+  return mongoose.connection.readyState === 1
+}
+
+export const oracleCheck = async (oracledb) => {
+  const pong = await oracledb.ping()
+  if (!pong) return false
+
+  return true
+}
+
+export default (dependencies) => {
+  const router = new Router()
+  router.get('/', async (req, res) => {
+    try {
+      const promises = dependencies.map(dependency => {
+        switch (dependency.type) {
+          case 'mongo': {
+            return mongoCheck(dependency.instance)
+          }
+          case 'oracle': {
+            return oracleCheck(dependency.instance)
+          }
+          case 'api': {
+            return apiCheck(dependency.URL)
+          }
+
+          default: return Promise.reject
+        }
+      })
+
+      const statuses = await Promise.all(promises)
+
+      res.status(OK).json(dependencies.map((dependency, i) => {
+        return { [dependency.name]: statuses[i] }
+      }))
+    } catch (e) {
+      console.log(e)
+      res.status(SERVICE_UNAVAILABLE).json({})
+    }
+  })
+  return router
 }
--- src/playground.js
+++ src/playground.js
@@ -2,19 +2,16 @@ import mongoose from 'mongoose'
 
 import { apiCheck, mongoCheck } from './index'
 
-
-// promisify mongoose
-mongoose.Promise = global.Promise
-
-// connect to mongo db
-mongoose.connect('mongodb://admin:admin@devdocker01.surrey.ca/mejai-development', { server: { socketOptions: { keepAlive: 1 } } })
-mongoose.connection.on('error', () => {
-  console.log('unable to connect to database')
-})
-
-const abc = async () => {
+const mCheck = async () => {
+  // promisify mongoose
+  mongoose.Promise = global.Promise
+  // connect to mongo db
+  await mongoose.connect('mongodb://admin:admin@devdocker01.surrey.ca/mejai-development', { server: { socketOptions: { keepAlive: 1 } } })
+  await mongoose.connection.on('error', () => {
+    console.log('unable to connect to database')
+  })
   const xxx = await mongoCheck(mongoose)
   console.log(xxx)
 }
 
-abc()
+const mongooseCheck = async
