9c87e399f3dddad383e6af901fb245a8cbec24f0
Stanley288 <stanley.zhao@surrey.ca>
2016-11-08 22:49:12 -0800

    feat: basic checks (#3)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
+import { OK } from 'http-status'
 import fetch from 'node-fetch'
 
-if (!global._babelPolyfill) {
-  require('babel-polyfill')
-}
-
 export const apiCheck = async (url) => {
-  const response = await fetch(url)
+  try {
+    const response = await fetch(url)
 
-  if (!response.ok) return false
-
-  return true
+    return response.ok
+  } catch (err) {
+    return false
+  }
 }
 
-export const mongoCheck = async (mongoose) => {
-  return (mongoose.connection.readyState === 1)
+export const mongoCheck = async mongoose => mongoose.connection.readyState === 1
+
+export const oracleCheck = async (oracledb) => {
+  try {
+    const pong = await oracledb.ping()
+
+    return pong
+  } catch (err) {
+    return false
+  }
+}
+
+export default dependencies => async (req, res) => {
+  const promises = Object.keys(dependencies).map((key) => {
+    switch (dependencies[key].type) {
+      case 'mongo':
+        return mongoCheck(dependencies[key].instance)
+
+      case 'oracle':
+        return oracleCheck(dependencies[key].instance)
+
+      case 'api':
+        return apiCheck(dependencies[key].url)
+
+      default: return Promise.reject()
+    }
+  })
+
+  const statuses = await Promise.all(promises)
+
+  const health = Object.keys(dependencies)
+    .reduce((acc, dependency, i) => ({ ...acc, [dependency]: statuses[i] }), {})
+
+  res.status(OK).json(health)
 }
